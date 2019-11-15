14adddec0fbe348b80ab69e1eaadf44606c9c067
bkrant <gladiolusi70@gmail.com>
2019-11-15 13:42:59 -0500

    names bst

--- names/names.py
+++ names/names.py
@@ -1,5 +1,45 @@
 import time
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value #this is the root node
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value == self.value:
+            return
+        elif value < self.value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+            
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        else:
+            if target < self.value:
+                if self.left == None:
+                    return False
+                else:
+                    return self.left.contains(target)
+            else:
+                if self.right == None:
+                    return False
+                else:
+                    return self.right.contains(target)
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -10,11 +50,20 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+bst = BinarySearchTree(names_1[0])
+for name in names_1[1:]:
+    bst.insert(name)
+
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
